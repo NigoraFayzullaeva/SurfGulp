25313b4257f355c0265088e3f0fb1f33e5eb0abb
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2020-12-20 14:14:47 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -30,4 +30,13 @@ $(function () {
         asNavFor: '.surf-slider',
         focusOnSelect: true
     });
+
+    $('.travel--slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrow-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrow-right.svg" alt="">',
+        asNavFor: '.slider-dotshead',
+
+    });
 });
