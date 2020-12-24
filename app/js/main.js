5c655667627917b7a6d64141cde3cda6f630d1be
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2020-12-24 20:52:11 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -72,6 +72,12 @@ $(function () {
 
     });
 
-    let summ =  ;
+    $('.quantity-button').on('click', function(){
+      let summ = $('.nights').val() * $('.summ').data('nights') + ($('guests').val() - 1) * $('.summ').data('guests');
+      $('.summ').html('$' + summ);
+    });
+
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('guests').val() - 1) * $('.summ').data('guests');
     $('.summ').html('$' + summ);
 });
+
