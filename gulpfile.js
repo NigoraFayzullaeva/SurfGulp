a009ed4f49b93216d98777f9703fedf60e09ac6b
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-01-09 20:46:51 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('css', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/animate.css/animate.css',
   ])
     .pipe(concat('_libs.scss'))
     .pipe(gulp.dest('app/scss'))
@@ -45,7 +46,8 @@ gulp.task('script', function(){
 
 gulp.task('js', function(){
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js'
+    'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/wow.js/dist/wow.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
